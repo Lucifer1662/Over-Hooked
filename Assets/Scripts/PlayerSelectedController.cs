d278f3710055074580704c532cb7028388d1486a
Luke Hawkins <l.hawk99@hotmail.com>
2020-09-25 16:50:31 +1000

    made menu rezieable

---
+++ Assets/Scripts/PlayerSelectedController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelectedController : MonoBehaviour
+{
+    [System.NonSerialized]
+    public CharacterParameters playerCharactersitc = null;
+
+    public GameObject playerSelected;
+    public GameObject notSelected;
+    private void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        if (playerCharactersitc == null){
+            playerSelected.SetActive(false);
+            notSelected.SetActive(true);
+        }
+        else{
+            playerSelected.SetActive(true);
+            notSelected.SetActive(false);
+            playerSelected.GetComponent<Text>().text = "Join as Controller " + (playerCharactersitc.number+1);
+        }
+    }
+
+}
