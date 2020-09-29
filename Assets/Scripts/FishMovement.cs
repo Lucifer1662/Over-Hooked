33c9cf3e93ad2e885bbc37ae086b8c529f3bb24a
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2020-09-29 15:22:55 +1000

    Fish Movement Experiment

---
+++ Assets/Scripts/FishMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FishMovement : MonoBehaviour
+{
+
+    public float movementTick = 3;
+    private float tickCountdown;
+
+    public float maxDistance;
+    private Vector3 target;
+    private NavMeshAgent nav;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tickCountdown -= Time.deltaTime;
+        if (tickCountdown < 0.0f)
+        {
+            
+            move();
+            tickCountdown = movementTick;
+        }
+    }
+
+    void move(){
+        float currentX = this.transform.position.x;
+        float currentZ = this.transform.position.z;
+
+        float newX = currentX + Random.Range(-maxDistance, maxDistance);
+        float newZ = currentZ + Random.Range(-maxDistance, maxDistance);
+
+        target = new Vector3 (newX, this.transform.position.y, newZ);
+
+        nav.SetDestination(target);
+    }
+}
