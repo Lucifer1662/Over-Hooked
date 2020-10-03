99a23f842b18293f65d2af51561bfc8dac1880ab
jess-hammer <59108399+jess-hammer@users.noreply.github.com>
2020-10-03 15:23:22 +1000

    Linked score to player, changed island layout a bit

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
     public int score;
+	public Text scoreDisplay;
 
+	private void Start ()
+	{
+		scoreDisplay.text = score.ToString ();
+	}
 
-    public void AddPoint() {
+	public void AddPoint() {
         score++;
-    }
+		scoreDisplay.text = score.ToString ();
+	}
 
     public int GetScore() {
         return score;
