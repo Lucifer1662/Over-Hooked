d8b54f1b8ebee0cfbbc46c1f1a554fd72ee8c969
Xiaochen Hou <hou.x@student.unimelb.edu.au>
2020-10-05 22:41:07 +0800

    working on the cometobait script

---
+++ Assets/Scripts/moveTowards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTowards : MonoBehaviour
+
+{
+    public float speed = 3;
+    private GameObject curHook;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        curHook = GameObject.FindGameObjectWithTag("Hook");
+        if (curHook != null){
+            GetComponent<FishMovement>().enabled = false;
+            transform.position = Vector3.MoveTowards(transform.position, curHook.transform.position, speed * Time.deltaTime);
+        }else{
+            GetComponent<FishMovement>().enabled = true;
+        }
+        
+    }
+}
