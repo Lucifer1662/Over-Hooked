8334567c6addc8c0e22a8ad805b98894bc2587eb
l.hawk99@hotmail.com <l.hawk99@hotmail.com>
2020-10-05 15:57:03 +1100

    added level complete scene change if you succeed or fail

---
+++ Assets/Scripts/IfPlayerScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ThreshHoldEvent : UnityEvent { }
+public class IfPlayerScore : MonoBehaviour
+{
+
+    public PlayerScore score;
+    public int threshhold;
+    [SerializeField]
+    public ThreshHoldEvent isAbove;
+    [SerializeField]
+    public ThreshHoldEvent isBelow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void TestPlayerScoreThreshhold()
+    {
+        if (score.score >= threshhold)
+        {
+            isAbove.Invoke();
+        }
+        else {
+            isBelow.Invoke();
+        }
+    }
+}
