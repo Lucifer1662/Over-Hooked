07fbb52507823fce686ed02f673bfaf9102059c7
jess-hammer <59108399+jess-hammer@users.noreply.github.com>
2020-10-09 10:42:40 +1100

    Added cricket sounds for the night level and experimented with disabling movement at the beginning

---
+++ Assets/Scripts/DisableEnableComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableEnableComponent : MonoBehaviour
+{
+	public MonoBehaviour component;
+	public bool disableOnStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (disableOnStart) {
+			component.enabled = false;
+		}
+    }
+
+	// Update is called once per frame
+	private void disableComponent ()
+	{
+		component.enabled = false;
+	}
+
+	private void enableComponent ()
+	{
+		component.enabled = true;
+	}
+}
