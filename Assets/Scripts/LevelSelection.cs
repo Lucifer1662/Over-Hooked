902f2a92dedaffe2820a693c016e202227d895bd
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2020-10-10 19:35:31 +1100

    Test level selection buttons

---
+++ Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("Jackson Test 2");
+    }
+
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene("Jackson Test 2");
+    }
+
+    public void LevelThree()
+    {
+        SceneManager.LoadScene("Jackson Test 2");
+    }
+}
