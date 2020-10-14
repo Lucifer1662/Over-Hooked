16fad74d30807f260827fd8f58704f89847e957f
jess-hammer <59108399+jess-hammer@users.noreply.github.com>
2020-10-14 14:54:24 +1100

    Updated functionality for main menu start and quit buttons

---
+++ Assets/ChangeSceneWithDelay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneWithDelay : MonoBehaviour
+{
+	public string scene;
+	public float delay;
+	private bool isActive = false;
+	
+
+	// Update is called once per frame
+	void Update()
+    {
+        if (isActive) {
+			delay -= Time.deltaTime;
+		}
+		if (delay <= 0) {
+			SceneManager.LoadScene (scene, LoadSceneMode.Single);
+		}
+    }
+
+	public void changeScene ()
+	{
+		isActive = true;
+	}
+}
