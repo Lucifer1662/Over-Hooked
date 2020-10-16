2066f5fb5b5a689eddece3ec5a9ea728309a3dea
l.hawk99@hotmail.com <l.hawk99@hotmail.com>
2020-10-16 13:40:47 +1100

    added saving levels

---
+++ Assets/Scripts/EnableIfLevelUnlocked.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableIfLevelUnlocked : MonoBehaviour
+{
+    public GameObject unlocked;
+    public GameObject locked;
+    public int level;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (LevelProgress.GetLevelProgress() >= level)
+        {
+            unlocked.SetActive(true);
+            locked.SetActive(false);
+            GetComponent<Button>().interactable = true;
+        }
+        else {
+            unlocked.SetActive(false);
+            locked.SetActive(true);
+            GetComponent<Button>().interactable = false;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelProgress.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelProgress : MonoBehaviour
+{
+    private static readonly string LevelProgressName = "LevelProgress";
+    public void SetLevelProgress(int level) {
+        PlayerPrefs.SetInt(LevelProgressName, level);
+    }
+
+    public static int GetLevelProgress() {
+        return PlayerPrefs.GetInt(LevelProgressName);
+    }
+}
