d2abbb73fc4e470ac81f5f558b9262bf06dcdf77
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2020-10-16 23:01:33 +1100

    Fix the issue with stuck in tutorial

--- Assets/Scripts/OnUpdate.cs
+++ Assets/Scripts/OnUpdate.cs
@@ -10,7 +10,7 @@ public class OnUpdate : MonoBehaviour
     [SerializeField]
     public OnUpdateEvent onUpdate;
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
         onUpdate.Invoke();
     }
