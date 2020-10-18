547750b8aa7e797717722b2a5a1d5402f99a46b4
l.hawk99@hotmail.com <l.hawk99@hotmail.com>
2020-10-18 15:03:20 +1100

    added pause menu to scenes

---
+++ Assets/Scripts/ReloadSceneWithDelay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadSceneWithDelay : MonoBehaviour
+{
+
+
+	public void reloadScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+	}
+
+	
+}
