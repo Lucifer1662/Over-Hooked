55548c8000528b26e2eb8aaf12e05ab941e234fa
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2020-10-20 21:48:21 +1100

    Fix score display of resume window

---
+++ Assets/TrackCurrentScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackCurrentScore : MonoBehaviour
+{
+    public Text currentScoreDisplay;
+	public Text resumeScoreDisplay;
+
+    public void DisplayCurrentScore()
+    {
+        resumeScoreDisplay.text = currentScoreDisplay.text;
+    }
+}
