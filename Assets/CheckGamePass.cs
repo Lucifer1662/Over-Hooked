8124062bebce369f7271138579a61c3cdd740a4d
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2020-10-29 18:07:02 +1100

    Try to embed credit animation into main menu

---
+++ Assets/CheckGamePass.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckGamePass : MonoBehaviour
+{
+    private float delay = 18;
+    public GameObject credit;
+    public GameObject title;
+    public GameObject menu;
+
+    public GameObject startButton;
+    public GameObject InstructionButton;
+    public GameObject quitButton;
+
+    public int testSwitch;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the status of game pass, for testing only
+        this.GetComponent<SaveGamePass>().SetGamePass(testSwitch);
+
+        if (this.GetComponent<SaveGamePass>().GetGamePass() == 1){
+            Debug.Log(this.GetComponent<SaveGamePass>().GetGamePass());
+            credit.SetActive(true);
+            SetMenuTransparent();
+            menu.SetActive(false);
+        }else{
+            credit.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay -= Time.deltaTime;
+        if (delay <= 10 && delay > 0) {
+            TitleFadeOut(2);
+		}
+		if (delay <= 0) {
+            credit.SetActive(false);
+            menu.SetActive(true);
+            TitleFadeIn(2);
+            MenuFadeIn(2);
+
+            if (title.GetComponent<Image>().color.a == 255) {
+                this.gameObject.SetActive(false);
+		    }
+		}
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+    private void TitleFadeOut(float speed){
+        Image titleImage = title.GetComponent<Image>();
+        if (titleImage.color.a > 0){
+            titleImage.color = new Color(titleImage.color.r, titleImage.color.g, titleImage.color.b, titleImage.color.a - (speed * Time.deltaTime));
+        }
+    }
+
+    private void TitleFadeIn(float speed){
+        Image titleImage = title.GetComponent<Image>();
+        titleImage.color = new Color(titleImage.color.r, titleImage.color.g, titleImage.color.b, titleImage.color.a + (speed * Time.deltaTime));
+    }
+
+    private void SetMenuTransparent(){
+
+        List<GameObject> buttons = new List<GameObject>{startButton, InstructionButton, quitButton};
+        foreach (GameObject item in buttons)
+        {
+            Image buttonImage = item.GetComponent<Image>();
+            Text buttonText = item.transform.GetChild (0).gameObject.GetComponent<Text>();
+
+            buttonImage.color = new Color(buttonImage.color.r, buttonImage.color.g, buttonImage.color.b, 0);
+            buttonText.color = new Color(buttonText.color.r, buttonText.color.g, buttonText.color.b, 0);
+        }
+    }
+
+    private void MenuFadeIn(float speed){
+
+        List<GameObject> buttons = new List<GameObject>{startButton, InstructionButton, quitButton};
+        foreach (GameObject item in buttons)
+        {
+            Image buttonImage = item.GetComponent<Image>();
+            Text buttonText = item.transform.GetChild (0).gameObject.GetComponent<Text>();
+
+            buttonImage.color = new Color(buttonImage.color.r, buttonImage.color.g, buttonImage.color.b, buttonImage.color.a + (speed * Time.deltaTime));
+            buttonText.color = new Color(buttonText.color.r, buttonText.color.g, buttonText.color.b, buttonText.color.a + (speed * Time.deltaTime));
+        }
+    }
+}
---
+++ Assets/SaveGamePass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGamePass : MonoBehaviour
+{
+    private static readonly string GamePassName = "GamePass";
+    public void SetGamePass(int identifier) {
+        PlayerPrefs.SetInt(GamePassName, identifier);
+    }
+
+    public int GetGamePass() {
+        return PlayerPrefs.GetInt(GamePassName);
+    }
+}
